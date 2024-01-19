3d992f175ac2521ddf2423735e2da72b7e1ea602
vbouq <v.bouquet707@gmail.com>
2024-01-19 12:06:57 -0800

    adds pages and routing

--- Project3DreamHues/main_app/models.py
+++ Project3DreamHues/main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+from datetime import date
 # Create your models here.
+FEELING = (
+  # for a dropdown menu in regards to feeling of dream
+  ('G', 'Good'),
+  ('O', 'Okay'),
+  ('N', 'Not Good'),
+  ('B', 'Bad')
+)
+
+TYPE = (
+  ('N', 'Nightmare')
+  ('A', 'Amazing')
+  ('W', 'Weird')
+)
+
+
+class Dreams(models.Model):
+  date = models.DateField('Date of Dream')
+  name = models.CharField(max_length=100)
+  about = models.TextField(max_length=500)
+  feeling = models.CharField(
+    max_length=1,
+    choices=FEELING,
+    default=FEELING[0][0]
+  )
+  dream_type = models.CharField(
+    max_length=1,
+    choices=TYPE,
+    default=TYPE[0][1]
+  )
