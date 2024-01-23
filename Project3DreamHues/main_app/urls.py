1bf74611d35a38b90984c5ee3b17467cab4d36b2
ariellepollock <ariellecpollock@gmail.com>
2024-01-23 09:05:05 -0600

    add CUD urls and views, add create dream link in nav

--- Project3DreamHues/main_app/urls.py
+++ Project3DreamHues/main_app/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('dreams/', views.dreams_index, name='index'),
   path('dreams/<int:dream_id>', views.dreams_detail, name='detail'),
+  path('dreams/create', views.DreamCreate.as_view(), name='dreams_create'),
+  path('dreams/<int:pk>/update', views.DreamUpdate.as_view(), name='dreams_update'),
+  path('dreams/<int:pk>/delete', views.DreamDelete.as_view(), name='dreams_delete'),
   # USER THINGS
   # log in page
   path('login/', views.login, name='login'),
