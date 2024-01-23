dbd3de530cb070b3d549604ce491217abeccd61d
JacobL <Jacob_Larranaga@yahoo.com>
2024-01-22 18:32:51 -0700

    Fixed background bug

--- Project3DreamHues/main_app/urls.py
+++ Project3DreamHues/main_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.home, name='home'),
@@ -11,4 +13,4 @@ urlpatterns = [
   path('signup/', views.signup, name='signup'),
   # log in page
   path('login/', views.login, name='login'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
