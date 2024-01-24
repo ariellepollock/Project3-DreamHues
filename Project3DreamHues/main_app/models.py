395b8793a33bb202948d701abc81dd78833434fa
ariellepollock <ariellecpollock@gmail.com>
2024-01-24 15:25:42 -0600

    form function

--- Project3DreamHues/main_app/models.py
+++ Project3DreamHues/main_app/models.py
@@ -22,16 +22,16 @@ TYPE = (
   ('W', 'Weird')
 )
 
-class Palette(models.Model):
-  # Assuming colors are in HEX format
-  color1 = models.CharField(max_length=7)  
-  color2 = models.CharField(max_length=7)
-  color3 = models.CharField(max_length=7)
-  color4 = models.CharField(max_length=7)
-  color5 = models.CharField(max_length=7)
+# class Palette(models.Model):
+#   # Assuming colors are in HEX format
+#   color1 = models.CharField(max_length=7)  
+#   color2 = models.CharField(max_length=7)
+#   color3 = models.CharField(max_length=7)
+#   color4 = models.CharField(max_length=7)
+#   color5 = models.CharField(max_length=7)
 
-  def __str__(self):
-    return f'{self.color1}, {self.color2}, {self.color3}, {self.color4}, {self.color5}'
+#   def __str__(self):
+#     return f'{self.color1}, {self.color2}, {self.color3}, {self.color4}, {self.color5}'
 
 class Dream(models.Model):
   date = models.DateField('Date of Dream')
