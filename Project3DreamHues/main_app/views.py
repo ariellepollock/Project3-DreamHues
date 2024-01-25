bd48481ea75b9c1a312b012dc050623d345f7057
ariellepollock <ariellecpollock@gmail.com>
2024-01-25 07:49:23 -0600

    fix image upload

--- Project3DreamHues/main_app/views.py
+++ Project3DreamHues/main_app/views.py
@@ -107,17 +107,17 @@ def add_photo(request, dream_id):
           bucket = os.environ['S3_BUCKET']
           s3.upload_fileobj(photo_file, bucket, key)
           url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-          print(f"URL: {url}")
+          Photo.objects.create(url=url, dream_id=dream_id)
+          # print(f"URL: {url}")
 
+          # # Imgix API Palette Generation
+          # palette = get_imgix_palette(url)
+          # print(f"Imgix API Response: {palette}")
 
-          # Imgix API Palette Generation
-          palette = get_imgix_palette(url)
-          print(f"Imgix API Response: {palette}")
-
-          if palette:
-                return render(request, 'dreams/add_photo.html', {'dream_id': dream_id, 'url': url, 'palette': palette})
-          else:
-                print('Imgix API error: Palette is None')
+          # if palette:
+          #       return render(request, 'dreams/add_photo.html', {'dream_id': dream_id, 'url': url, 'palette': palette})
+          # else:
+          #       print('Imgix API error: Palette is None')
 
         except Exception as e:
             print('woah nelly! an error occurred uploading your file')
